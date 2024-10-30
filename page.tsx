9426acb51c49c0c4f519a2c902a63d89edc7626a
SM-Hassaan-Qadri <qadrismhassaan@gmail.com>
2024-10-30 21:05:59 +0500

    Add files via upload
    In Next.js, routes refer to the pages and paths within an application, allowing users to navigate between different views. Each file in the pages directory automatically becomes a route based on its filename and path structure. For example, a file named 
    **pages/about.js** 
    creates a route accessible at /about. Next.js supports file-based routing, dynamic routes (e.g., [id].js for variable paths), and API routes (pages/api) to handle backend logic. This system enables intuitive, scalable routing without the need for complex configuration.

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import link from "next/link"
+export default function page(){
+    return(
+        <h1>
+             this is elon musk. he is the co founder of tesla </h1>
+    
+    
+    )
+}
+page()
